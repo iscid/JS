9241bc330315a585657222531a8bc1138dea7d51
iscid <iscivandelgado@gmail.com>
2026-08-17 19:15:59 -0600

    Inicializando Array Methos, trabajando con Some

---
+++ ArrayMethods/js/01_some.js
@@ -0,0 +1,20 @@
+// Some:
+
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+meses.forEach((mes) => {
+  console.log("Mes: ", mes);
+});
