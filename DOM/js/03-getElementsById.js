83380212c3744696bc50232cd765fe99cc6ca583
iscid <iscivandelgado@gmail.com>
2026-08-21 21:59:23 -0600

    Se agrega 03-getElementById.js

---
+++ DOM/js/03-getElementsById.js
@@ -0,0 +1,7 @@
+// Sólo devuelve un resultado, debido a que las buenas prácticas indican que los ids son únicos.
+
+const formulario = document.getElementById("formulario");
+console.log(formulario); // devuelve todo el formulario
+
+const card = document.getElementById("card");
+console.log(card); // devuelve null ya que no existe ese id
